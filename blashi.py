ad4a03bbcaacfcaa9cd255d31204fcff24422dcb
Bryan Wagner <jbw.nasa@gmail.com>
2017-10-15 18:46:40 -0500

    Fist try at creating the GUI.  Added some simple pngs for the game

--- blashi.py
+++ blashi.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import json
+import sys
 from pprint import pprint
+import pygame
 
 class Map(object):
     horizontal_border = "--------------------------------------"
@@ -18,11 +20,43 @@ class Map(object):
         with open(f) as data_file:    
             self.board = json.load(data_file)
 
+def _start_gui():
+
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+ 
+    pygame.init()
+    size = (700, 500)
+    screen = pygame.display.set_mode(size)
+ 
+    pygame.display.set_caption("Blashi")
+    img = pygame.image.load('pics/cannon.png')
+    done = False
+ 
+    clock = pygame.time.Clock()
+ 
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+ 
+        screen.fill(WHITE)
+        screen.blit(img, pygame.rect.Rect(0,0, 16, 16))
+        pygame.display.flip()
+ 
+        clock.tick(60)
+ 
+    pygame.quit()
+
 def start_game(map_file):
     game_map = Map()
     game_map.load(map_file)
 
     game_map.output()
+
+    _start_gui()
     
 def main(story_file):
     start_game(story_file)
