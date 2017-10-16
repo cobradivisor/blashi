33eefec7b47a9c4164407eecda7aa10662b5c06b
Bryan Wagner <jbw.nasa@gmail.com>
2017-10-15 19:41:49 -0500

    Starting to create grid for board

--- blashi.py
+++ blashi.py
@@ -3,6 +3,8 @@ import json
 import sys
 from pprint import pprint
 import pygame
+import os
+from pygame.rect import Rect as Rectangle
 
 class Map(object):
     horizontal_border = "--------------------------------------"
@@ -20,15 +22,29 @@ class Map(object):
         with open(f) as data_file:    
             self.board = json.load(data_file)
 
-def _start_gui():
+def _create_grid(width,height,number_of_rows, number_of_columns):
+    grid = []
+    grid_height = height/number_of_rows
+    grid_width = width/number_of_columns
+    for r in range(number_of_rows):
+        row = []
+        for c in range(number_of_columns):
+            row.append(Rectangle(c*grid_width,r*grid_height,grid_width,grid_height))
+         
+        grid.append(row)
+    return grid
 
-    BLACK = (0, 0, 0)
+def _size_board(board):
+    return (2,2)
+
+def _start_gui(board):
+
     WHITE = (255, 255, 255)
-    GREEN = (0, 255, 0)
-    RED = (255, 0, 0)
- 
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
     pygame.init()
-    size = (700, 500)
+    width = 700
+    height = 500 
+    size = (width, height)
     screen = pygame.display.set_mode(size)
  
     pygame.display.set_caption("Blashi")
@@ -36,6 +52,10 @@ def _start_gui():
     done = False
  
     clock = pygame.time.Clock()
+
+    number_of_rows,number_of_columns = _size_board(board)
+
+    grid = _create_grid(width,height,number_of_rows, number_of_columns)
  
     while not done:
         for event in pygame.event.get():
@@ -43,7 +63,7 @@ def _start_gui():
                 done = True
  
         screen.fill(WHITE)
-        screen.blit(img, pygame.rect.Rect(0,0, 16, 16))
+        screen.blit(img, Rectangle(0,0, 16, 16))
         pygame.display.flip()
  
         clock.tick(60)
@@ -56,7 +76,7 @@ def start_game(map_file):
 
     game_map.output()
 
-    _start_gui()
+    _start_gui(game_map)
     
 def main(story_file):
     start_game(story_file)
--- tests/test_blashi.py
+++ tests/test_blashi.py
@@ -1,6 +1,7 @@
 import pytest
+import blashi
 from blashi import Map
-
+from pygame.rect import Rect as Rectangle
 def test_balishi_main(capsys):
     game_map = Map()
     game_map.load("tests/maps/empty_map.json")  
@@ -9,3 +10,11 @@ def test_balishi_main(capsys):
     expected_output = game_map.horizontal_border + "\n" + game_map.horizontal_border
     assert expected_output in out
 
+def test_balishi_create_grid_with_one_element():
+    grid = blashi._create_grid(100,100,1,1)
+    assert grid == [[Rectangle(0,0,100,100)]]
+
+def test_balishi_create_grid_with_2_X_2():
+    grid = blashi._create_grid(100,100,2,2)
+    print grid
+    assert grid == [ [Rectangle(0,0,50,50), Rectangle(50,0,50,50)], [Rectangle(0,50,50,50), Rectangle(50,50,50,50)] ]
